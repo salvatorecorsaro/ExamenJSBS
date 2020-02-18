1d372243b651001a2a430c5cb1bea70de4410476
salvatorecorsaro <salvatore.corsaro@live.com>
2020-02-18 10:58:51 +0100

    fixed formats

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 var listElement = document.querySelector("#profiles");
 
 function fetchData(url, callback) {
-    let request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.responseType = 'json';
-    request.onreadystatechange = function () {
-        if (request.readyState === 4 && request.status === 200) {
-            callback(request.response);
-        }
-    };
-    request.send();
+  let request = new XMLHttpRequest();
+  request.open('GET', url);
+  request.responseType = 'json';
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(request.response);
+    }
+  };
+  request.send();
 }
 
 function showItems(response) {
-    for (let i = 0; i < response.results.length; i++) {
-        listElement.innerHTML += `
+  for (let i = 0; i < response.results.length; i++) {
+    listElement.innerHTML += `
 
         
        
@@ -32,36 +32,27 @@ function showItems(response) {
               <div class="row">
               <div class="container-fluid">
               <div class="row">
-              <div class="col-12 col-md-5">
-              
+              <div class="col-12 col-md-5">              
               <p><i class="fas fa-phone"></i> ${response.results[i].phone} </p>
-
               </div>
               <div class="col-12 col-md-7">
               <p><i class="far fa-user"></i> ${response.results[i].login.username} </p>
               </div>
               </div>
               </div>
-              </div>                 
-           
-              
+              </div>              
            <p><i class="far fa-envelope"></i> ${response.results[i].email}</p>
            <p><i class="fas fa-birthday-cake"></i> ${response.results[i].dob.date.substring(8, 10)}-${response.results[i].dob.date.substring(5, 7)}-${response.results[i].dob.date.substring(0, 4)}</p>
            <p><i class="fas fa-map"></i> ${response.results[i].location.street.number}, ${response.results[i].location.street.name}, ${response.results[i].location.city}, ${response.results[i].location.state}</p>
            </div>
-            </div>
+          </div>
           </div>
         </div>
-      </div>
-    
+      </div>    
       <div class="col-0 col-md-1">
-      </div>
-        
-        
+      </div>       
     `;
-    }
+  }
 }
-
-
 
 fetchData('https://randomuser.me/api/?results=8', showItems);
